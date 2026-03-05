f75bae5d46d9909decbfbfd5ab49ed7e79f15dc2
jwa91 <122170013+jwa91@users.noreply.github.com>
2026-03-05 13:09:37 +0100

    Add trnscrb cask v0.1.0

---
+++ Casks/trnscrb.rb
@@ -0,0 +1,13 @@
+cask "trnscrb" do
+  version "0.1.0"
+  sha256 "fa613c527165647e1988166e6f87a4bf90d8c35d48f0185813bb32b661715bbc"
+
+  url "https://github.com/jwa91/trnscrb/releases/download/v#{version}/trnscrb-#{version}.dmg"
+  name "trnscrb"
+  desc "Drop audio, PDFs, and images onto your menu bar to get markdown back"
+  homepage "https://github.com/jwa91/trnscrb"
+
+  depends_on macos: ">= :sonoma"
+
+  app "trnscrb.app"
+end
