34e77686e09bbe541633c09a6d7322ecdeb53a1a
jwa91 <122170013+jwa91@users.noreply.github.com>
2026-03-05 21:37:50 +0100

    Update trnscrb cask to 0.1.1

--- Casks/trnscrb.rb
+++ Casks/trnscrb.rb
@@ -1,6 +1,6 @@
 cask "trnscrb" do
-  version "0.1.0"
-  sha256 "fa613c527165647e1988166e6f87a4bf90d8c35d48f0185813bb32b661715bbc"
+  version "0.1.1"
+  sha256 "29c6c574696bfe0f2bf1923b0ebeffbfbd040632f7a6f5e9e94281cd2ac4fb82"
 
   url "https://github.com/jwa91/trnscrb/releases/download/v#{version}/trnscrb-#{version}.dmg"
   name "trnscrb"
@@ -11,6 +11,9 @@ cask "trnscrb" do
 
   app "trnscrb.app"
 
+  # TODO: Remove this postflight block once Apple has accepted our notarization
+  # submission. After that, Gatekeeper will recognize the app and the quarantine
+  # bypass is no longer needed.
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-d", "com.apple.quarantine", "#{appdir}/trnscrb.app"],
